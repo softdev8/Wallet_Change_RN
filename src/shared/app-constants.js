caebe4f6a65ef40872b189318318a3e620d68360
MobileDev418 <oncemoreforyou@outlook.com>
2018-03-02 17:28:55 +0200

    shared js

---
+++ src/shared/app-constants.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+const screenDimensions = Dimensions.get('window');
+
+ const AppConstants = {
+    ScreenDimensions: screenDimensions,
+    HeaderTitleWidth: screenDimensions.width - 120,
+    IsSmallDevice: screenDimensions.height <= 568,
+    BundleId: DeviceInfo.getBundleId(),
+    DeviceId:DeviceInfo.getUniqueID(),
+};
+export default AppConstants;
---
+++ src/shared/app-styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+export const mainTextColor = '#fff';
+
+export const mainBgColor = '#7e57c2';
+export const secondaryBgColor = '#7c4dff';
+export default StyleSheet.create({
+    layout: {
+        backgroundColor: mainBgColor,
+        flex: 1
+    },
+    container:
+        {
+            paddingLeft: 12.5,
+            paddingRight: 12.5,
+            paddingTop: 12.5,
+            paddingBottom: 12.5,
+        },
+    mainTextColor: { color: mainTextColor },
+    mainButtonColor: { color: secondaryBgColor },
+    fullWidth: {
+        position: 'absolute',
+        left: 0, right: 0, top: 0, bottom: 0
+    },
+}
+);
