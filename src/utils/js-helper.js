ec9d5c3348f72c194d83b4cdc7441674ac78ae00
Mobile Developer <oncemoreforyou@outlook.com>
2018-05-11 02:40:54 +0800

    Update js-helper.js

--- src/utils/js-helper.js
+++ src/utils/js-helper.js
@@ -2,7 +2,6 @@ export const leftPad = (str, fillChar, length = 20) => {
     str = '' + str;
     return `${fillChar.repeat(Math.max(length - str.length, 0))}${str}`;
 }
-
 export const rightPad = (str, fillChar, length = 20) => {
     str = '' + str;
     return `${str}${fillChar.repeat(Math.max(length - str.length, 0))}`;
