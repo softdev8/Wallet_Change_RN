cc892cfd3e2f8ebaba295c25aa8e83741cf68dcc
Mobile Developer <oncemoreforyou@outlook.com>
2018-05-22 10:37:32 +0800

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 
 // let appUrl = 'http://162.243.166.143:3000/api';
 let appUrl = 'http://myworks.life:3005/api';
-//if (__DEV__)
- //   appUrl = 'http://192.168.2.33:3000/api';
+if (__DEV__)
+   appUrl = 'http://192.168.2.33:3000/api';
 
 export default {
     api_url: appUrl,
