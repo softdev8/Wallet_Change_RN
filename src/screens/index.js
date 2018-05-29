0e1fda5cdef342929c08acff9cb0ccc8e117ac32
Mobile Developer <oncemoreforyou@outlook.com>
2018-05-30 02:47:26 +0800

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,4 @@
 import LoginScreen from './login.screen';
 import RegiterScreen from './register.screen';
-import MainScreen from './main.screen';
 
-export { LoginScreen, RegiterScreen, MainScreen }
+export { LoginScreen, RegiterScreen }
