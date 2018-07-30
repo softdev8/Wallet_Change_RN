b36a179f8dfe662ef7b2c4a2de4a053fe4305f3f
Mobile Developer <oncemoreforyou@outlook.com>
2018-07-29 22:53:30 -0400

    Update history.action.js

--- src/actions/history.action.js
+++ src/actions/history.action.js
@@ -28,3 +28,26 @@ export function query(type, dateFrom, dateTo) {
     };
 
 }
+export function select(type, dateFrom, dateTo) {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: Actions.fetching, payload: { domain: 'history' } });
+            let authToken = AuthService.getToken();
+            let queryStr = `trans/query?type=${type}&from=${dateFrom}`;
+            if (dateTo)
+                queryStr += `&to=${dateTo}`;
+
+            const data = await ApiClient.get(queryStr, authToken);
+
+            dispatch({ type: Actions.query, payload: data });
+
+            dispatch({ type: Actions.fetched, payload: { domain: 'history' } });
+        }
+        catch (err) {
+            console.log(err);
+            dispatch({ type: Actions.error, payload: { domain: 'history', error: err } });
+        }
+    };
+
+}
+
