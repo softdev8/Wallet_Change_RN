6041327c01466f45df8023e860dc781f40cb78e3
Mobile Developer <oncemoreforyou@outlook.com>
2018-08-21 21:48:00 -0400

    Update index.js

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import { StackNavigator } from "react-navigation";
 
 import ProfileScreen from './profile.screen';
-
+import SettingScreen from './setting.screen';
+import EditProfileScreen from './editprofile.screen';
 
 export default (DrawNav = StackNavigator({
     profile: { screen: ProfileScreen },
+    setting: { screen: SettingScreen },
+    editprofile: { screen: EditProfileScreen },
 }));
